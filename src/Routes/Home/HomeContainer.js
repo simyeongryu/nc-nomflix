26d7aeb8127c5dc005a673d768338bc77356382a
simyeongryu <rsm0503@gmail.com>
2020-04-24 20:13:16 +0900

    5.3

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,6 +1,7 @@
 // 스테이트 값을 가진 컴포넌트
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { movieApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -10,6 +11,39 @@ export default class extends React.Component {
     error: null,
     loading: true
   };
+  // 각각의 요청을 분리된 get으로 만들어서 할 수 있다.
+  // ex) getNowPlaying ..
+  // componentDidMount 안에는 this를 이용 호출
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await movieApi.nowPlaying();
+
+      const {
+        data: { results: upcoming }
+      } = await movieApi.upcoming();
+
+      const {
+        data: { results: popular }
+      } = await movieApi.popular();
+
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch (e) {
+      console.log(e);
+      this.setState({
+        error: "Can't find movies information."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
 
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,6 +1,7 @@
 // 스테이트 값을 가진 컴포넌트
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -11,8 +12,37 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated }
+      } = await tvApi.topRated();
+      const {
+        data: { results: popular }
+      } = await tvApi.popular();
+      const {
+        data: { results: airingToday }
+      } = await tvApi.airingToday();
+      this.setState({
+        topRated,
+        popular,
+        airingToday
+      });
+    } catch (e) {
+      console.log(e);
+      this.setState({
+        error: "Can't find movies information."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, error, loading } = this.state;
+    console.log(this.state);
     return (
       <TVPresenter
         topRated={topRated}
